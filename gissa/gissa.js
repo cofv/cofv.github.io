8d09b05d100db28ba01895695890e0cea323aa7e
Sebastian Näs <sena0220@storsjogymnasiet.se>
2023-04-06 10:51:00 +0200

    nästanklar

---
+++ gissa/gissa.js
@@ -0,0 +1,11 @@
+const slumptal = Math.floor(math.random() * 9) + 1;
+let antalgissningar = 0;
+function gissaTal(){
+    console.log("click")
+    let textfelt_gissa = document.getElementById("gissa").value;
+    let gissa_int = Number(textfelt_gissa);
+    let vinn = false;
+    let p_svar = document.getElementById("p_svar");
+    console.log("textfelt=" + textfelt_gissa)
+}
+console.log("slumptalet" + slumptal)
