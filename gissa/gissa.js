133d8fe30646ca539b86e37f24bbb19fa09e2186
Sebastian Näs <sena0220@storsjogymnasiet.se>
2023-04-06 11:37:05 +0200

    Jsahhh

--- gissa/gissa.js
+++ gissa/gissa.js
@@ -7,5 +7,26 @@ function gissaTal(){
     let vinn = false;
     let p_svar = document.getElementById("p_svar");
     console.log("textfelt=" + textfelt_gissa)
+    if (antalgissningar < 3){
+        if(slumptal === gissa_int){
+            console.log(`RÄTT! antalgissningar= $(antalgisningar)`);
+            vinn = true;
+            p_svar.innerHTML= `YOUR A WINNER!`;
+            antalgisningar = 3;
+        }
+        else {
+            console.log(`FEL! antalgissningar= $(antalgisningar)`);
+            if (antalgisningar === 2)
+            p_svar.innerHTML=`LOOSER!`;
+            else
+            p_svar.innerHTML=`$(textfelt_gissa) är FEL! Gissning nr $(antalgisningar+1) av 3`;
+            document.getElementById("gissa").value="";
+            document-getElementById("gissa").focus();
+        }
+        antalgisningar++;
+    }
+    else{
+        p_svar.innerHTML=`LOOSER`!
+    }
 }
 console.log("slumptalet" + slumptal)
