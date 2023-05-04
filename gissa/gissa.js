039581794306ec869111b1bd4caec45fb5b8af9b
Sebastian Näs <sena0220@storsjogymnasiet.se>
2023-05-04 11:41:38 +0200

    fixade js code gissa chatgpt

--- gissa/gissa.js
+++ gissa/gissa.js
@@ -1,32 +1,39 @@
-const slumptal = Math.floor(math.random() * 9) + 1;
+const slumptal = Math.floor(Math.random() * 9) + 1;
 let antalgissningar = 0;
-function gissaTal(){
+console.log("slumptal:" + slumptal)
+
+function gissaTal() {
     console.log("click")
     let textfelt_gissa = document.getElementById("gissa").value;
     let gissa_int = Number(textfelt_gissa);
-    let vinn = false;
     let p_svar = document.getElementById("p_svar");
     console.log("textfelt=" + textfelt_gissa)
-    if (antalgissningar < 3){
-        if(slumptal === gissa_int){
-            console.log(`RÄTT! antalgissningar= $(antalgisningar)`);
-            vinn = true;
-            p_svar.innerHTML= `YOUR A WINNER!`;
-            antalgisningar = 3;
+    
+    if (antalgissningar < 3) {
+        if (slumptal === gissa_int) {
+            console.log(`RÄTT! antalgissningar= ${antalgissningar}`);
+            p_svar.innerHTML= "YOU'RE A WINNER!";
+            antalgissningar = 3;
+        } else {
+            console.log(`FEL! antalgissningar= ${antalgissningar}`);
+            if (antalgissningar === 2) {
+                p_svar.innerHTML = "LOSER!";
+                antalgissningar = 3;
+            } else {
+                p_svar.innerHTML = `${textfelt_gissa} är FEL! Gissning nr ${antalgissningar + 1} av 3`;
+                document.getElementById("gissa").value = "";
+                document.getElementById("gissa").focus();
+                antalgissningar++;
+            }
         }
-        else {
-            console.log(`FEL! antalgissningar= $(antalgisningar)`);
-            if (antalgisningar === 2)
-            p_svar.innerHTML=`LOOSER!`;
-            else
-            p_svar.innerHTML=$(textfelt_gissa)+ "är FEL! Gissning nr " +$(antalgisningar+1)+ "av 3";
-            document.getElementById("gissa").value="";
-            document-getElementById("gissa").focus();
-        }
-        antalgisningar++;
+    } else {
+        p_svar.innerHTML = "LOSER!";
     }
-    else{
-        p_svar.innerHTML= "LOSSER!"
+
+    // Disable further guessing after the game has ended
+    if (antalgissningar === 3) {
+        document.getElementById("gissa").disabled = true;
     }
 }
-console.log("slumptalet" + slumptal)
+
+console.log("slumptalet " + slumptal);
